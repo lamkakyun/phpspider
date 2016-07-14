188e595b7c45675ae88180c5a4f402ba54f5afc7
lamkakyun <lamkakyun@gmail.com>
2016-07-15 00:59:35 +0800

    testing

--- config/autoload/local.php
+++ config/autoload/local.php
@@ -3,8 +3,8 @@
 return array(
     'website' => array(
         'log.lamkakyun.com' => array(
-            'username' => 'lamkakyun',
-            'password' => '13427199'
+            'username' => '',
+            'password' => ''
         )
     )
 );
