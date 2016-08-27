58d14cd22aa33eff12298efd8b82716758ba7cc0
lamkakyun <lamkakyun@gmail.com>
2016-08-27 18:44:01 +0800

    testing

--- module/Admin/src/Admin/Controller/PublicController.php
+++ module/Admin/src/Admin/Controller/PublicController.php
@@ -14,9 +14,45 @@ use Zend\View\Model\ViewModel;
 class PublicController extends CommonController
 {
 
-    public function loginAction() {
+    /**
+     * 登陆页面
+     * @return ViewModel
+     */
+    public function loginAction()
+    {
         $model = new ViewModel();
         $model->setTerminal(true); // 不设置模板
         return $model;
     }
+
+
+    /**
+     * 登陆操作
+     */
+    public function doLoginAction()
+    {
+
+    }
+
+
+    /**
+     * 注册页面
+     * @return ViewModel
+     */
+    public function registerAction()
+    {
+        $model = new ViewModel();
+        $model->setTerminal(true);
+        return $model;
+    }
+
+
+    /**
+     * 注册操作
+     */
+    public function doRegisterAction()
+    {
+
+    }
+
 }
